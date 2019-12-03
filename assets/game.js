f84dad2391f8f397526af4ddfb422e97c0dfb8c0
Nicholas Loperena <nicholasloperena@gmail.com>
2019-12-03 17:59:20 -0500

    cleaned up the game.js file, created a win or lose condition for the game. Working on making the logic work for adding up the value to the points system

--- assets/game.js
+++ assets/game.js
@@ -1,22 +1,56 @@
-//computer chooses a score between 50 to 100
-//..
-var points = 0;
-var score = getValue(50, 100);
-var blue = getValue(1, 10);
-var silver = getValue(3, 12);
-var green = getValue(1, 3);
-var violet = getValue(1, 4);
 
+
+//start off by making variables
+var points = 0;
+//getValue function is called to pick a number between 50 and 100
+//and assigns the value to score.
+var score = getValue(50, 100);
+
+
+//asigning values to gems
+var gems = {
+    blue: {
+        value: getValue(1,2)},
+    silver: {
+        value: getValue(3,6)},
+    green: {
+        value: getValue(7,10)},
+    violet: {
+        value: getValue(11,20)}
+    
+}
+
+
+// gets a random number between 2 values
 function getValue(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
+//------------------------------------------------------
 
-if (green === silver || violet || blue && silver === violet || blue && violet === blue) {
+// checks to see if you win or lose
+function winOrLose(){
+    if (score === points){
+        $('points').text('You win! score: ' + score);
+    } else if(score < points){
+        $('#points').text('You lose! Your score is greater than ' + score);
+    }
+}
+//-------------------------------------------------------
+
+// adds the values into the points variable
+function addPoints(){
 
 }
 
+
+//testing values and logging them
 console.log("score: " + score)
-console.log("blue: " + blue)
-console.log("silver: " + silver)
-console.log("violet: " + violet)
-console.log("green: " + green)
+console.log("blue: " + gems.blue)
+console.log("silver: " + gems.silver)
+console.log("violet: " + gems.violet)
+console.log("green: " + gems.green)
+console.log("points: " + points)
+
+
+
+
