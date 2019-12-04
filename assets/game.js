092404a01d0bef6f1314cd19ea8c15d10d5ab5eb
Nicholas Loperena <nicholasloperena@gmail.com>
2019-12-03 19:42:53 -0500

    almost done with the logic, now making the startgame function

--- assets/game.js
+++ assets/game.js
@@ -4,21 +4,48 @@
 var points = 0;
 //getValue function is called to pick a number between 50 and 100
 //and assigns the value to score.
-var score = getValue(50, 100);
+var score = 0;
 
 
 //asigning values to gems
 var gems = {
     blue: {
-        value: getValue(1,2)},
+        value: 0},
     silver: {
-        value: getValue(3,6)},
+        value: 0},
     green: {
-        value: getValue(7,10)},
+        value: 0},
     violet: {
-        value: getValue(11,20)}
+        value: 0}
     
 }
+
+startGame() = function() {
+    //resets points to 0 
+    points = 0;
+    //makes a new value for the score
+    score = getValue(50, 100);
+    // sets each value of the gem to a random number
+    gems.blue.value = getValue(1,2);
+    gems.silver.value = getValue(3,6);
+    gems.green.value = getValue(7,10);
+    gems.violet.value = getValue(11,20);
+    // takes you to the game by switching out the HTML on the page
+    $('')
+}
+
+
+
+
+//testing values and logging them
+console.log("score: " + score)
+console.log("blue: " + gems.blue)
+console.log("silver: " + gems.silver)
+console.log("violet: " + gems.violet)
+console.log("green: " + gems.green)
+console.log("points: " + points)
+
+
 
 
 // gets a random number between 2 values
@@ -38,19 +65,13 @@ function winOrLose(){
 //-------------------------------------------------------
 
 // adds the values into the points variable
-function addPoints(){
-
+function addPoints(gemClick){
+    //actually adds the points value to the gem clicked on the stream
+    points + gemClick.value;
+    //grabs the element with the #points ID and  displays the points variable's value
+    $('#points').text(points)
+    //runs the function to check if you win or lose
+    winOrLose();
 }
-
-
-//testing values and logging them
-console.log("score: " + score)
-console.log("blue: " + gems.blue)
-console.log("silver: " + gems.silver)
-console.log("violet: " + gems.violet)
-console.log("green: " + gems.green)
-console.log("points: " + points)
-
-
 
 
